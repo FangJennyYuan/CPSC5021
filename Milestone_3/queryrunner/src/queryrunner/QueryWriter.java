1ad31144862af0cd9e7d9ab661a67635b0a7a6da
Celeste <celestebroderick@gmail.com>
2018-12-01 22:41:15 -0800

    methods to write query results to csv

---
+++ Milestone_3/queryrunner/src/queryrunner/QueryWriter.java
@@ -0,0 +1,41 @@
+package querywriter;
+import java.io.*;
+
+public class QueryWriter
+{
+   public static void writeToCSV(String filename, String[] header, 
+         String[][] results) throws FileNotFoundException
+   {
+      File queryCSV = new File(filename);
+      PrintWriter outputFile = new PrintWriter(queryCSV);
+      
+      // Print column headers
+      for (int i = 0; i < header.length; i++)
+      {
+         outputFile.print(header[i]);
+         outputFile.print(DELIMITER);
+      }
+      outputFile.println();
+      
+      // Print query results
+      for (int i = 0; i < results.length; i++)
+      {  
+         for (int j = 0; j < results[i].length; j++)
+         {
+            outputFile.print(results[i][j]);
+            outputFile.print(DELIMITER);
+         }
+         outputFile.println();
+      }
+      outputFile.close();
+   }
+   
+   public static void writeToCSV(String[] header, String[][] results) 
+         throws FileNotFoundException
+   {
+      writeToCSV(DEFAULT_CSV_FILENAME, header, results);
+   }
+   
+   private static final String DELIMITER = ",";
+   private static final String DEFAULT_CSV_FILENAME = "query.csv";
+}
